c588abd593c1e5652ea626c6b0393b5ba6d5c755
tantry-chiranthan <127607005+tantry-chiranthan@users.noreply.github.com>
2023-11-23 10:01:57 +0530

    Create Day1_m_Name.cs

---
+++ Day1_m_Name.cs
@@ -0,0 +1,136 @@
+[10:01] K, Bhuvan
+review1.cs
+
+int d; // elapsed time in days
+ 
+int elapsedTimeInDays
+
+************************************************************************
+
+review2.cs
+
+var dataFromDb = db.GetFromService().ToList(); // Get List of employees
+ 
+var employeeList = employeeService.GetEmployeeList();
+
+************************************************************************
+
+review3.cs
+
+int iCounter;
+
+string strFullName;
+
+DateTime dModifiedDate;
+ 
+int counter
+
+string fullName;
+
+DateTime modifiedDate;
+
+************************************************************************
+
+review4.cs
+
+public bool IsShopOpen(string pDay, int pAmount)
+
+{
+
+    // some logic
+
+}
+ 
+public bool IsShopOpen(string Day, int Amount)
+
+{
+
+    // some logic
+
+}
+
+************************************************************************
+
+review5.cs
+
+const int DAYS_IN_WEEK = 7;
+
+const int daysInMonth = 30;
+ 
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+
+var Artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+ 
+bool EraseDatabase() {}
+
+bool Restore_database() {}
+ 
+class animal {}
+
+class Alpaca {}
+ 
+const int DAYS_IN_WEEK = 7;
+
+const int DAYS_IN_MONTH = 30;
+ 
+var songs = new List<string> { 'Back In Black', 'Stairway to Heaven', 'Hey Jude' };
+
+var artists = new List<string> { 'ACDC', 'Led Zeppelin', 'The Beatles' };
+ 
+bool EraseDatabase() {}
+
+bool RestoreDatabase() {}
+ 
+class Animal {}
+
+class Alpaca {}
+
+************************************************************************
+
+review6.cs
+
+public class Employee
+
+{
+
+    public Datetime sWorkDate { get; set; } // get set Start Working Date
+
+    public Datetime modTime { get; set; } // get set Modification Time
+
+}
+ 
+public class Employee
+
+{
+
+    public Datetime startWorkDate { get; set; } // get set Start Working Date
+
+    public Datetime modificationTime { get; set; } // get set Modification Time
+
+}
+
+************************************************************************
+
+review7.cs
+
+var employeephone;
+ 
+public double CalculateSalary(int workingdays, int workinghours)
+
+{
+
+    // some logic
+
+}
+ 
+var employeePhone;
+ 
+public double CalculateSalary(int workingDays, int workingHours)
+
+{
+
+    // some logic
+
+}
+
+************************************************************************
